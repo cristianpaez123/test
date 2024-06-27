36099d231d05b134fcf8a58ea7c54b75dc43f8d6
cristianpaez123 <cristianpaezguerrero@gmail.com>
2024-06-26 22:07:46 -0500

    se crea la logica para la autenticacion

--- app/src/main/java/com/example/test/iu/view/MainActivity.kt
+++ app/src/main/java/com/example/test/iu/view/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.test.iu.view
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -10,12 +11,18 @@ import androidx.lifecycle.Observer
 import com.example.test.R
 import com.example.test.databinding.ActivityMainBinding
 import com.example.test.iu.viewModel.MainViemModel
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+
 
     private val viemModel: MainViemModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +35,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = Firebase.auth
         setupObserver()
+        binding.btnLogIn.setOnClickListener {
+            logIn(binding.editTextEmail.text.toString(), binding.editTextPassword.text.toString())
+        }
     }
-    private fun setupObserver(){
+
+    private fun setupObserver() {
         viemModel.dataModel.observe(this, Observer {
             binding.textData.text = it.text
         })
+    }
+
+    private fun logIn(email: String, password: String) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            FirebaseAuth.getInstance()
+                .signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        binding.textData.text = getIdUser()
+                    } else {
+                        Toast.makeText(this@MainActivity, "pailas", Toast.LENGTH_LONG).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this@MainActivity, "perrito ingrese datos", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun getIdUser(): String {
+        val user = auth.currentUser
+        return user?.uid ?: ""
     }
 }
